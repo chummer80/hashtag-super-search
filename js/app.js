1c4ba58c0da972fc535331218e835e1f13a7e9a9
chummer80 <mike.c.chu@gmail.com>
2014-04-12 04:24:29 -0700

    trying to get twitter token

--- js/app.js
+++ js/app.js
@@ -1,18 +1,46 @@
 $(document).ready(function() {
+	var twitterKey = 'TcTBMur5S4sxfJOO2qD0UMnjE';
+	var twitterSecret = '21ctTZsvmWvuUG2624h2kOhzRjfuEPbDefwtztHkNhRWy1YVA8';
+	var twitterCredential = twitterKey + ':' + twitterSecret;
+	var twitterCredentialBase64 = btoa(twitterCredential);
+	
+	// $('#twitter_test').click(function() {
+		// console.log("Twitter test: searching for top #NBA tweet");
+		// var queryData = {
+			// q: '#nba',
+			// result_type: 'popular',
+			// count: 1
+		// };
+		// $.getJSON('https://api.twitter.com/1.1/search/tweets.json', queryData)
+			// .done(function() {
+				// console.log("Successful query for #NBA");
+			// })
+			// .fail(function() {
+				// console.log("Failed to query for #NBA");
+			// });
+	// });
+	
+	
 	$('#twitter_test').click(function() {
-		console.log("Twitter test: searching for top #NBA tweet");
-		var queryData = {
-			q: '#nba',
-			result_type: 'popular',
-			count: 1
-		};
-		$.getJSON('https://api.twitter.com/1.1/search/tweets.json', queryData)
-			.done(function() {
-				console.log("Successful query for #NBA");
+		console.log("Getting Twitter credentials");
+		$.ajax({
+			url: 'https://api.twitter.com/oauth2/token',
+			type: 'POST',
+			headers: {
+				Authorization: 'Basic ' + twitterCredentialBase64,
+				'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+			},
+			data: {
+				grant_type: 'client_credentials'
+			},
+			datatype: 'json'
+		})
+			.done(function(data) {
+				console.log("Successfully got Twitter bearer token");
+			
 			})
 			.fail(function() {
-				console.log("Failed to query for #NBA");
+				console.log("Failed to get Twitter bearer token");
 			});
 	});
-
 });
