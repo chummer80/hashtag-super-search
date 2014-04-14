1a360b3ba2b11f9ab1e9bb58efad61cc74af33e8
chummer80 <mike.c.chu@gmail.com>
2014-04-14 05:22:38 -0700

    Add logic to start a hashtag search
    get input string, trim it, then make sure it's a valid hashtag

--- js/app.js
+++ js/app.js
@@ -1,8 +1,77 @@
 $(document).ready(function() {
-	var twitterKey = 'TcTBMur5S4sxfJOO2qD0UMnjE';
-	var twitterSecret = '21ctTZsvmWvuUG2624h2kOhzRjfuEPbDefwtztHkNhRWy1YVA8';
-	var twitterCredential = twitterKey + ':' + twitterSecret;
+	/********************
+	*	Constants
+	********************/
+	
+	var ENTER_KEY_CODE = 13;
+	
+	// hashtags must start with a letter, and may only contain letters, digits, and underscore.
+	var VALID_HASHTAG_PATTERN = /^[a-z]\w*$/i;
+	
+	var TWITTER_KEY = 'TcTBMur5S4sxfJOO2qD0UMnjE';
+	var TWITTER_SECRET = '21ctTZsvmWvuUG2624h2kOhzRjfuEPbDefwtztHkNhRWy1YVA8';
+	
+	var twitterCredential = TWITTER_KEY + ':' + TWITTER_SECRET;
 	var twitterCredentialBase64 = btoa(twitterCredential);
+	
+	/********************
+	*	Functions
+	********************/
+	
+	var trimSearchString = function trimSearchString(searchString) {
+		// remove leading and trailing white space
+		searchString = searchString.trim();
+		
+		// remove leading pound signs if they were accidentally typed in.
+		var i = 0;
+		while (searchString[i] === '#' && i < searchString.length) {
+			i++;
+		}
+		searchString = searchString.slice(i);
+		
+		return searchString;
+	};
+	
+	// validate the user inputted hashtag
+	var searchIsValid = function searchIsValid(searchString) {
+		if (searchString.length === 0) 
+		{
+			alert("Search string cannot be blank");
+			return false;
+		}
+		
+		if (searchString.search(VALID_HASHTAG_PATTERN) == -1) 
+		{
+			console.log("VALID_HASHTAG_PATTERN was not found in " + searchString);
+			alert("Hashtags must start with a letter and can only contain letters, numbers, and underscore.");
+			return false;
+		}
+		
+		return true;
+	};
+	
+	var startNewSearch = function startNewSearch() {
+		var searchString = $('#search_textbox').val();
+		searchString = trimSearchString(searchString);
+		
+		if (searchIsValid(searchString)) {
+			
+		}
+	};
+	
+	/********************
+	*	Event Handlers
+	********************/
+	
+	$('#search_textbox').keydown(function() {
+		if (event.which == ENTER_KEY_CODE) {
+			startNewSearch();
+		}
+	});
+	
+	$('#search_button').click(function() {
+		startNewSearch();
+	});
 	
 	// $('#twitter_test').click(function() {
 		// console.log("Twitter test: searching for top #NBA tweet");
